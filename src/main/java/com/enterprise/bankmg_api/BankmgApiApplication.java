5c8b067d6fbf37b06fbdca5fff81f23a4d5346c9
paulo.brito <paulocesarti01@gmail.com>
2026-02-07 17:54:00 -0300

    feat: arquivos iniciais

---
+++ src/main/java/com/enterprise/bankmg_api/BankmgApiApplication.java
@@ -0,0 +1,13 @@
+package com.enterprise.bankmg_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankmgApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankmgApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enterprise/bankmg_api/BankmgApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterprise.bankmg_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankmgApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
